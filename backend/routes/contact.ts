79bec7f3d6b78f2aa28f9808ef9cf2a48dbb5f35
Hairum Qureshi <hqureshi@udel.edu>
2024-07-05 15:26:06 -0400

    created route to get all the current user's contacts and implemented logic for retrieving said logic

--- backend/routes/contact.ts
+++ backend/routes/contact.ts
@@ -1,9 +1,11 @@
 import express from "express";
-import { addContact } from "../controllers/contact";
+import { addContact, getContacts } from "../controllers/contact";
 import { authenticated } from "../middleware/authentication";
 
 const router = express.Router();
 
 router.post("/add", authenticated, addContact);
+
+router.get("/all", authenticated, getContacts);
 
 export default router;
