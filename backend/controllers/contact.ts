a6483251e2fe395d0a1ce0a3cc5f302f1e64921a
Hairum Qureshi <hqureshi@udel.edu>
2024-07-06 09:48:13 -0400

    implemented another check to make sure the user_id passed in is in a valid Mongoose ID format which - this stopped the error involving user_ids not being in a valid format if the user entered gibberish

--- backend/controllers/contact.ts
+++ backend/controllers/contact.ts
@@ -1,46 +1,59 @@
 import { Request, Response } from "express";
 import User from "../models/user";
 import Contact from "../models/contact";
+import mongoose from "mongoose";
 
 const addContact = async (req: Request, res: Response) => {
 	const { user_id } = req.body;
+	// TODO - Will need to check if the user_id passed in is a valid Mongo ID
 	try {
 		const curr_uid: string = req.cookies.decoded_uid;
 
-		if (user_id === curr_uid) {
-			return res
-				.status(400)
-				.json({ message: "You can't add yourself as a contact" });
-		} else {
-			const findUserByUID = await User.findById({ _id: user_id });
-			if (findUserByUID) {
-				// Check if the current user already has a contact with this specific user
-				const checkHasContact = await Contact.find({
-					user_id: curr_uid,
-					contacts: {
-						$in: [user_id]
-					}
-				});
-				if (checkHasContact.length > 0) {
-					return res
-						.status(400)
-						.json({ message: "You already have a contact with this user" });
-				} else {
-					// Add the user ID to the current user's contact 'contacts' array
-					const updatedContact = await Contact.findOneAndUpdate(
-						{ user_id: curr_uid },
-						{
-							$push: {
-								contacts: user_id
-							}
-						}
-					);
-
-					return res.status(200).json(updatedContact);
-				}
+		if (mongoose.isValidObjectId(user_id)) {
+			if (user_id === curr_uid) {
+				return res
+					.status(400)
+					.json({ message: "You can't add yourself as a contact" });
 			} else {
-				res.status(404).json({ message: `User not found with ID: ${user_id}` });
+				const findUserByUID = await User.findById({ _id: user_id });
+				if (findUserByUID) {
+					// Check if the current user already has a contact with this specific user
+					const checkHasContact = await Contact.find({
+						user_id: curr_uid,
+						contacts: {
+							$in: [user_id]
+						}
+					});
+					if (checkHasContact.length > 0) {
+						return res
+							.status(400)
+							.json({ message: "You already have a contact with this user" });
+					} else {
+						// Add the user ID to the current user's contact 'contacts' array
+						const updatedContact = await Contact.findOneAndUpdate(
+							{ user_id: curr_uid },
+							{
+								$push: {
+									contacts: user_id
+								}
+							},
+							{ new: true }
+						)
+							.populate({
+								path: "contacts",
+								select: "-password -__v"
+							})
+							.select("-_id contacts");
+						return res.status(200).json([updatedContact]);
+					}
+				} else {
+					res
+						.status(404)
+						.json({ message: `User not found with ID: ${user_id}` });
+				}
 			}
+		} else {
+			res.status(400).json({ message: "Invalid user ID" });
 		}
 	} catch (error) {
 		console.log(
@@ -54,12 +67,11 @@ const getContacts = async (req: Request, res: Response) => {
 	const curr_uid: string = req.cookies.decoded_uid;
 	try {
 		const contacts = await Contact.find({ user_id: curr_uid })
-			.select("-_id contacts")
 			.populate({
 				path: "contacts",
 				select: "-password -__v"
-			});
-
+			})
+			.select("-_id contacts");
 		return res.status(200).json(contacts);
 	} catch (error) {
 		console.log(
