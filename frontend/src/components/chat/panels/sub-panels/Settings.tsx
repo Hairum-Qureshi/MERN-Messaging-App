18b9908c4a378f827acd5379f525e45797a6b692
Hairum Qureshi <hqureshi@udel.edu>
2024-07-05 22:37:43 -0400

    added sign out button

--- frontend/src/components/chat/panels/sub-panels/Settings.tsx
+++ frontend/src/components/chat/panels/sub-panels/Settings.tsx
@@ -1,4 +1,7 @@
-import { faArrowLeft } from "@fortawesome/free-solid-svg-icons";
+import {
+	faArrowLeft,
+	faRightFromBracket
+} from "@fortawesome/free-solid-svg-icons";
 import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
 import { useRef } from "react";
 import { Link } from "react-router-dom";
@@ -22,16 +25,29 @@ export default function Settings({ updatePageStatus }: Props) {
 
 	return (
 		<div className="border border-blue-500 h-screen w-1/4 bg-slate-800 flex flex-col">
-			<div
-				onClick={() => updatePageStatus("settings")}
-				className="text-xl border w-1/3 border-white-400 p-1 bg-slate-500 rounded hover:cursor-pointer active:bg-slate-700 m-4"
-			>
-				<Link to="/conversations">
-					<div className="flex items-center justify-center">
-						<FontAwesomeIcon icon={faArrowLeft} />
-						<span className="ml-2 text-lg">Go Back</span>
-					</div>
-				</Link>
+			<div className="flex items-center">
+				<div
+					onClick={() => updatePageStatus("settings")}
+					className="text-xl border w-1/3 border-white-400 p-1 bg-slate-500 rounded hover:cursor-pointer active:bg-slate-700 m-4"
+				>
+					<Link to="/conversations">
+						<div className="flex items-center justify-center">
+							<FontAwesomeIcon icon={faArrowLeft} />
+							<span className="ml-2 text-lg">Go Back</span>
+						</div>
+					</Link>
+				</div>
+				<div
+					onClick={() => updatePageStatus("settings")}
+					className="text-xl border w-1/3 border-white-400 p-1 bg-blue-500 rounded hover:cursor-pointer active:bg-blue-700 m-4 ml-auto"
+				>
+					<Link to="/">
+						<div className="flex items-center justify-center">
+							<FontAwesomeIcon icon={faRightFromBracket} />
+							<span className="ml-2 text-lg">Sign Out</span>
+						</div>
+					</Link>
+				</div>
 			</div>
 			<div className="w-full m-2">
 				<h1 className="text-2xl font-semibold">Settings</h1>
