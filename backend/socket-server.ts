b58272e44b7561719be1858e06b499f33b58f3ff
Hairum Qureshi <hqureshi@udel.edu>
2024-07-06 06:51:52 -0400

    created main three functions for storing, removing, and retrieving users that connect/disconnect from socket.io connection

--- backend/socket-server.ts
+++ backend/socket-server.ts
@@ -1,12 +1,38 @@
 import { Server as HttpServer } from "http";
 import { Server as IOServer, Socket } from "socket.io";
 
+interface SocketData {
+	user_id: string;
+	socket_id: string;
+}
+
+let onlineUsers: SocketData[] = [];
+
+function addUser(user_id: string, socket_id: string) {
+	if (!onlineUsers.some((user: SocketData) => user.user_id === user_id)) {
+		onlineUsers.push({ user_id, socket_id });
+	}
+
+	return;
+}
+
+function removeUser(user_id: string) {
+	onlineUsers = onlineUsers.filter(
+		(user: SocketData) => user.user_id !== user_id
+	);
+}
+
+function getUser(user_id: string): SocketData {
+	return onlineUsers.find((user: SocketData) => user.user_id === user_id)!;
+}
+
 const initializeSocket = (server: HttpServer) => {
 	const io = new IOServer(server, {
 		cors: {
 			origin: ["http://localhost:5173"]
 		}
 	});
+
 	io.on("connection", (socket: Socket) => {
 		console.log("A user connected", socket.id);
 
