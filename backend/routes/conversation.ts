c19e4728072a03cc32a85dbf0de20c1bd8aa1d58
Hairum Qureshi <hqureshi@udel.edu>
2024-07-08 18:19:39 -0400

    included custom authenticated middleware in conversation.ts

--- backend/routes/conversation.ts
+++ backend/routes/conversation.ts
@@ -1,8 +1,9 @@
 import express from "express";
 import { getAllConversations } from "../controllers/conversation";
+import { authenticated } from "../middleware/authentication";
 
 const router = express.Router();
 
-router.get("/all", getAllConversations);
+router.get("/all", authenticated, getAllConversations);
 
 export default router;
