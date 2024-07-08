45a7fc6bcee48cb99f848fe74b7094021b00f3a9
Hairum Qureshi <hqureshi@udel.edu>
2024-07-08 09:42:53 -0400

    created useEffect hook calling the API endpoints for getting friend requests and friend requests the current user sent and returning them via state variables

--- frontend/src/hooks/useFriendRequest.ts
+++ frontend/src/hooks/useFriendRequest.ts
@@ -1,15 +1,24 @@
 import axios from "axios";
+import { FriendRequest, SentFriendRequest } from "../interfaces";
+import { useEffect, useState } from "react";
 
 interface Tools {
 	sendFriendRequest: (user_id: string) => void;
+	sentFriendRequests: SentFriendRequest[];
+	friendRequests: FriendRequest[];
 }
 
 export default function useFriendRequest(): Tools {
+	const [sentFriendRequests, setSentFriendRequests] = useState<
+		SentFriendRequest[]
+	>([]);
+	const [friendRequests, setFriendRequests] = useState<FriendRequest[]>([]);
+
 	async function sendFriendRequest(user_id: string) {
 		if (user_id) {
 			await axios
 				.post(
-					"http://localhost:3000/api/contacts/send-friend-request",
+					"http://localhost:3000/api/friend-requests/send",
 					{
 						user_id
 					},
@@ -18,16 +27,48 @@ export default function useFriendRequest(): Tools {
 					}
 				)
 				.then(response => {
-					// handle response
+					setSentFriendRequests(prev => [response.data, ...prev]);
 				})
 				.catch(error => {
-					console.log(error.response.data.message);
-					alert(error.response.data.message);
+					console.log(error);
+					// console.log(error.response.data.message);
+					// alert(error.response.data.message);
 				});
 		} else {
 			alert("Please provide a user ID");
 		}
 	}
 
-	return { sendFriendRequest };
+	useEffect(() => {
+		async function getFriendRequests() {
+			await axios
+				.get("http://localhost:3000/api/friend-requests/all", {
+					withCredentials: true
+				})
+				.then(response => {
+					setFriendRequests(response.data);
+				})
+				.catch(error => {
+					console.log(error);
+				});
+		}
+
+		async function getSentFriendRequests() {
+			await axios
+				.get("http://localhost:3000/api/friend-requests/all-sent", {
+					withCredentials: true
+				})
+				.then(response => {
+					setSentFriendRequests(response.data);
+				})
+				.catch(error => {
+					console.log(error);
+				});
+		}
+
+		getFriendRequests();
+		getSentFriendRequests();
+	}, []);
+
+	return { sendFriendRequest, friendRequests, sentFriendRequests };
 }
