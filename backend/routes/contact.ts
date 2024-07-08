42143733c9f56588fcc5b853a7d179b72eb34d48
Hairum Qureshi <hqureshi@udel.edu>
2024-07-08 06:38:04 -0400

    changed route from '/add' to '/send-friend-request'

--- backend/routes/contact.ts
+++ backend/routes/contact.ts
@@ -1,10 +1,10 @@
 import express from "express";
-import { addContact, getContacts } from "../controllers/contact";
+import { sendFriendRequest, getContacts } from "../controllers/contact";
 import { authenticated } from "../middleware/authentication";
 
 const router = express.Router();
 
-router.post("/add", authenticated, addContact);
+router.post("/send-friend-request", authenticated, sendFriendRequest);
 
 router.get("/all", authenticated, getContacts);
 
