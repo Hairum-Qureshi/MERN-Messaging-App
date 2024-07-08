cc047ff105d7e82484b4715e05cc0bc834c56545
Hairum Qureshi <hqureshi@udel.edu>
2024-07-08 05:45:45 -0400

    removed import and usage of now deleted 'Contact' model

--- backend/controllers/authentication.ts
+++ backend/controllers/authentication.ts
@@ -9,56 +9,55 @@ import dotenv from "dotenv";
 dotenv.config();
 colors.enable();
 
-function createCookie(user_id: mongoose.Types.ObjectId, res: Response) {
-	const payload = {
-		user_id
-	};
-	const secretKey: string = process.env.JWT_SECRET!;
-	const token = jwt.sign(payload, secretKey, { expiresIn: "3d" });
-	res.cookie("auth-session", token, { httpOnly: true, maxAge: 259200000 }); // 3 days in milliseconds
+async function createToken(user_id: mongoose.Types.ObjectId): Promise<string> {
+	try {
+		const payload = {
+			user_id
+		};
+
+		const secretKey: string = process.env.JWT_SECRET!;
+		const token: string = jwt.sign(payload, secretKey, { expiresIn: "3d" });
+
+		return token;
+	} catch (error) {
+		console.log("Error creating token:".red.bold, error);
+		throw new Error("Error creating token");
+	}
 }
 
 const sign_up = async (req: Request, res: Response) => {
 	const { first_name, last_name, email, password } = req.body;
 	try {
-		const user = await User.findOne({ email });
-		if (user) {
+		const existingUser = await User.findOne({ email });
+		if (existingUser) {
 			return res.status(400).json({ message: "User already exists" });
-		} else {
-			const salt = await bcrypt.genSalt(10);
-			const hashed_password = await bcrypt.hash(password, salt);
-			await User.create({
-				full_name: `${first_name.replace(
-					first_name[0],
-					first_name[0].toUpperCase()
-				)} ${last_name.replace(last_name[0], last_name[0].toUpperCase())}`,
-				email: email.toLowerCase(),
-				password: hashed_password
-			})
-				.then(created_user => {
-					// console.log(response);
-					if (created_user._id) {
-						createCookie(created_user._id, res);
-						return res
-							.status(201)
-							.json({ message: "User created successfully" });
-					}
-				})
-				.catch((error: Error) => {
-					console.log(
-						"<authentication.ts>".yellow.bold,
-						"error".red.bold,
-						error.toString().red.bold
-					);
-					return res.status(500).json(error);
-				});
 		}
+
+		const salt = await bcrypt.genSalt(10);
+		const hashed_password = await bcrypt.hash(password, salt);
+		const full_name = `${
+			first_name.charAt(0).toUpperCase() + first_name.slice(1)
+		} ${last_name.charAt(0).toUpperCase() + last_name.slice(1)}`;
+		const newUser = await User.create({
+			full_name,
+			email: email.toLowerCase(),
+			password: hashed_password
+		});
+
+		const token = await createToken(newUser._id);
+		res
+			.status(201)
+			.cookie("auth-session", token, {
+				httpOnly: true,
+				maxAge: 259200000 // 3 days in milliseconds
+			})
+			.json({ message: "User created successfully" });
 	} catch (error) {
 		console.log(
-			"<authentication.ts>".yellow.bold,
-			"error".red.bold,
+			"<authentication.ts> controller".yellow.bold,
 			(error as Error).toString().red.bold
 		);
+		res.status(500).json({ message: "Internal server error" });
 	}
 };
 
@@ -66,23 +65,27 @@ const sign_in = async (req: Request, res: Response) => {
 	const { email, password } = req.body;
 	try {
 		const user = await User.findOne({ email });
-		if (user) {
-			const isPasswordValid = await bcrypt.compare(password, user.password);
-			if (isPasswordValid) {
-				createCookie(user._id, res);
-				return res.status(200).json({ message: "User signed in successfully" });
-			} else {
-				return res.status(401).json({ message: "Invalid credentials" });
-			}
-		} else {
+		if (!user) {
 			return res.status(401).json({ message: "Invalid credentials" });
 		}
+
+		const isPasswordValid = await bcrypt.compare(password, user.password);
+		if (!isPasswordValid) {
+			return res.status(401).json({ message: "Invalid credentials" });
+		}
+
+		const token = await createToken(user._id);
+		res
+			.status(201)
+			.cookie("auth-session", token, { httpOnly: true, maxAge: 259200000 }) // 3 days in milliseconds
+			.json({ message: "Signed in successfully" });
 	} catch (error) {
 		console.log(
-			"<authentication.ts>".yellow.bold,
+			"<authentication.ts> controller".yellow.bold,
 			"error".red.bold,
 			(error as Error).toString().red.bold
 		);
+		res.status(500).json({ message: "Internal server error" });
 	}
 };
 
