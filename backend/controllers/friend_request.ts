4850967c1e62673286e190a415fdde6c00217615
Hairum Qureshi <hqureshi@udel.edu>
2024-07-08 08:20:21 -0400

    implemented MongoDB queries for specific data results

--- backend/controllers/friend_request.ts
+++ backend/controllers/friend_request.ts
@@ -8,64 +8,70 @@ colors.enable();
 
 const sendFriendRequest = async (req: Request, res: Response) => {
 	const { user_id } = req.body;
-
 	try {
 		const curr_uid: string = req.cookies.decoded_uid;
 
-		if (mongoose.isValidObjectId(user_id)) {
-			if (user_id === curr_uid) {
-				return res.status(400).json({
-					message: "You cannot add yourself as a friend!"
-				});
-			}
-		} else {
-			const findUserByUID = await User.findById({ _id: user_id });
-			if (findUserByUID) {
-				const findFriend = await User.findOne({
-					_id: curr_uid,
-					friends: { $elemMatch: { $eq: user_id } }
-				});
-
-				if (findFriend) {
-					return res.status(400).json({
-						message: "You already have this user as a friend"
-					});
-				} else {
-					const findPendingRequest = await FriendRequest.findOne({
-						$and: [{ sender: curr_uid }, { receiver: user_id }]
-					});
-
-					if (findPendingRequest) {
-						return res.status(400).json({
-							message:
-								"You already have a pending friend request with this user"
-						});
-					} else {
-						console.log("Friend request created!".yellow);
-						await FriendRequest.create({
-							sender: curr_uid,
-							receiver: user_id
-						})
-							.then(createdFriendRequest => {
-								return res.status(200).json(createdFriendRequest);
-							})
-							.catch(error => {
-								console.log("<friend_request.ts> controller", error.red.bold);
-								return res.status(500).json(error);
-							});
-					}
-				}
-			} else {
-				return res.status(404).json({
-					message: "User not found!"
-				});
-			}
+		// Check if user_id is a valid ObjectId
+		if (!mongoose.isValidObjectId(user_id)) {
+			console.log("Invalid user ID");
+			return res.status(400).json({ message: "Invalid user ID" });
 		}
+
+		// Check if the user is trying to add themselves
+		if (user_id === curr_uid) {
+			console.log("You cannot add yourself as a friend");
+			return res
+				.status(400)
+				.json({ message: "You cannot add yourself as a friend!" });
+		}
+
+		const findUserByUID = await User.findById(user_id);
+		if (!findUserByUID) {
+			console.log("User not found");
+			return res.status(404).json({ message: "User not found!" });
+		}
+
+		console.log("User found. Proceeding");
+
+		// Check if the current user already has this user as a friend
+		const findFriend = await User.findOne({
+			_id: curr_uid,
+			friends: { $elemMatch: { $eq: user_id } }
+		});
+
+		if (findFriend) {
+			console.log("You already have this user as a friend");
+			return res
+				.status(400)
+				.json({ message: "You already have this user as a friend" });
+		}
+
+		// Check if there is already a pending friend request
+		const findPendingRequest = await FriendRequest.findOne({
+			sender: curr_uid,
+			receiver: user_id
+		});
+
+		if (findPendingRequest) {
+			console.log("You already have a pending friend request with this user");
+			return res.status(400).json({
+				message: "You already have a pending friend request with this user"
+			});
+		}
+
+		// Create the friend request
+		const createdFriendRequest = await FriendRequest.create({
+			sender: curr_uid,
+			receiver: user_id
+		});
+
+		console.log("Friend request created!");
+		return res.status(200).json(createdFriendRequest);
 	} catch (error) {
-		console.log(
-			"<friend_request.ts> controller".yellow.bold,
-			(error as Error).toString().red.bold
-		);
+		console.log("<friend_request.ts> controller", (error as Error).toString());
+		return res
+			.status(500)
+			.json({ message: "Server error", error: (error as Error).toString() });
 	}
 };
 
@@ -74,7 +80,12 @@ const getFriendRequests = async (req: Request, res: Response) => {
 		const curr_uid: string = req.cookies.decoded_uid;
 		const pendingFR = await FriendRequest.find({
 			receiver: curr_uid
-		}).populate("sender");
+		})
+			.populate({
+				path: "sender",
+				select: "_id full_name profile_picture status_update"
+			})
+			.select("sender");
 		return res.status(200).json(pendingFR);
 	} catch (error) {
 		console.log(
@@ -90,13 +101,14 @@ const getAllPendingFriendRequests = async (req: Request, res: Response) => {
 
 		const pendingRequests = await FriendRequest.find({
 			sender: curr_uid,
-			$and: [
-				{
-					accepted: false,
-					rejected: false
-				}
-			]
-		}).populate("receiver");
+			accepted: false,
+			rejected: false
+		})
+			.populate({
+				path: "receiver",
+				select: "_id full_name profile_picture status_update"
+			})
+			.select("receiver");
 
 		if (pendingRequests && pendingRequests.length > 0) {
 			return res.status(200).json(pendingRequests);
