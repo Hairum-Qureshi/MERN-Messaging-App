9cc65a8171c6d04f19b326de80a730a35f06d86f
Hairum Qureshi <hqureshi@udel.edu>
2024-07-08 12:21:27 -0400

    created functions to handle retrieving friend request data and sending friend request data to the backend via socket.io

--- frontend/src/hooks/useSocketIO.ts
+++ frontend/src/hooks/useSocketIO.ts
@@ -1,7 +1,7 @@
 import { useEffect, useRef, useState } from "react";
 import io, { Socket } from "socket.io-client";
 import useAuthContext from "../contexts/authContext";
-import { SocketData, SocketTools } from "../interfaces";
+import { FriendRequest, SocketData, SocketTools } from "../interfaces";
 
 const socket_io = io("http://localhost:3000", { autoConnect: false });
 
@@ -9,6 +9,8 @@ export default function useSocketIO(): SocketTools {
 	const [activeUsers, setActiveUsers] = useState<SocketData[]>([]);
 	const { userData } = useAuthContext()!;
 	const socket = useRef<Socket>(socket_io);
+	const [incomingFriendRequest, setIncomingFriendRequest] =
+		useState<FriendRequest>();
 
 	useEffect(() => {
 		socket.current.connect();
@@ -26,11 +28,38 @@ export default function useSocketIO(): SocketTools {
 			});
 		});
 
+		socket.current.on(
+			"receive-friend-request",
+			(friendRequestData: FriendRequest) => {
+				setIncomingFriendRequest(friendRequestData);
+				// setIncomingFriendRequest(prev => [friendRequestData, ...prev]);
+				return () =>
+					socket?.current && socket?.current.off("receive-friend-request");
+			}
+		);
 		// Cleanup function to disconnect the socket when the component unmounts
 		return () => {
 			socket.current.disconnect();
 		};
 	}, [userData]);
 
-	return { activeUsers };
+	function sendFriendRequestEvent(
+		sender_uid: string,
+		receiver_uid: string,
+		sender_pfp: string,
+		sender_name: string,
+		sender_status: string
+	) {
+		if (socket.current.connected) {
+			socket.current.emit("send-friend-request", {
+				sender_uid,
+				receiver_uid,
+				sender_pfp,
+				sender_name,
+				sender_status
+			});
+		}
+	}
+
+	return { activeUsers, sendFriendRequestEvent, incomingFriendRequest };
 }
