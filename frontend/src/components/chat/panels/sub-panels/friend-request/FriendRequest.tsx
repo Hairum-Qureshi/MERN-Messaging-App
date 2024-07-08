ef21aca108508eef4ca0400efda3f0e0fa02cfe7
Hairum Qureshi <hqureshi@udel.edu>
2024-07-08 06:02:31 -0400

    created a new sub folder called 'friend-request' that contains components related towards the friend request page

---
+++ frontend/src/components/chat/panels/sub-panels/friend-request/FriendRequest.tsx
@@ -0,0 +1,35 @@
+import { faArrowLeft } from "@fortawesome/free-solid-svg-icons";
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+import { Link } from "react-router-dom";
+import PendingFRBlock from "./PendingFRBlock";
+
+interface Props {
+	updatePageStatus: (page: string) => void;
+}
+
+export default function FriendRequest({ updatePageStatus }: Props) {
+	return (
+		<div className="border border-blue-500 h-screen w-1/3 bg-slate-800 flex flex-col">
+			<div
+				onClick={() => updatePageStatus("friend_request")}
+				className="text-xl border w-1/3 border-white-400 p-1 bg-slate-500 rounded hover:cursor-pointer active:bg-slate-700 m-4"
+			>
+				<Link to="/conversations">
+					<div className="flex items-center justify-center">
+						<FontAwesomeIcon icon={faArrowLeft} />
+						<span className="ml-2 text-lg">Go Back</span>
+					</div>
+				</Link>
+			</div>
+			<h1 className="text-2xl font-semibold m-3">Your Friend Requests (1)</h1>
+			<div className="flex-grow overflow-y-auto">
+				<div className="p-5 text-xl text-slate-400 font-semibold text-center">
+					<h1>
+						It's lonely here! Check back later when you have a friend request
+					</h1>
+				</div>
+				{/* <PendingFRBlock /> */}
+			</div>
+		</div>
+	);
+}
