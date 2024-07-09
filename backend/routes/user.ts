177d4726a64adc8e5a465582145c42f345f4fee1
Hairum Qureshi <hqureshi@udel.edu>
2024-07-09 15:36:49 -0400

    created new user PATCH request route for user settings

--- backend/routes/user.ts
+++ backend/routes/user.ts
@@ -1,9 +1,11 @@
 import express from "express";
-import { getCurrentUser } from "../controllers/user";
+import { getCurrentUser, updateUserSettings } from "../controllers/user";
 import { authenticated } from "../middleware/authentication";
 
 const router = express.Router();
 
 router.get("/current", authenticated, getCurrentUser);
+
+router.patch("/settings", authenticated, updateUserSettings);
 
 export default router;
