426d36a9729fa32af5e6ca6dad5ff915ef5f860f
Hairum Qureshi <hqureshi@udel.edu>
2024-07-09 13:14:29 -0400

    implemented Axios requests for getting the conversation's uploaded media depending on the URL parameter so it dynamically changes via useEffect

--- frontend/src/hooks/useConversation.ts
+++ frontend/src/hooks/useConversation.ts
@@ -1,13 +1,17 @@
 import { useEffect, useState } from "react";
 import axios from "axios";
-import { Conversation } from "../interfaces";
+import { Conversation, Media } from "../interfaces";
 
 interface Tools {
 	conversations: Conversation[];
+	// chatMessages: Message[];
+	conversationMedia: Media[];
 }
 
-export default function useConversation() {
+export default function useConversation(): Tools {
 	const [conversations, setConversations] = useState<Conversation[]>([]);
+	const [conversationMedia, setConversationMedia] = useState<Media[]>([]);
+	// const [chatMessages, setChatMessages] = useState<Message[]>([]);
 
 	useEffect(() => {
 		async function getChats() {
@@ -26,5 +30,46 @@ export default function useConversation() {
 		getChats();
 	}, []);
 
-	return { conversations };
+	const chat_id: string | undefined = window.location.href.split("/").pop();
+	useEffect(() => {
+		async function getChatMessages() {
+			try {
+				// if (chat_id) {
+				// 	const response = await axios.get(
+				// 		`http://localhost:4000/api/messages/${chat_id}`
+				// 	);
+				// 	setChatMessages(response.data);
+				// }
+			} catch (error) {
+				console.log(error);
+			}
+		}
+
+		async function getChatMedia() {
+			try {
+				if (chat_id) {
+					await axios
+						.get(
+							`http://localhost:3000/api/conversations/${chat_id}/get-media`,
+							{
+								withCredentials: true
+							}
+						)
+						.then(response => {
+							setConversationMedia(response.data);
+						})
+						.catch(error => {
+							console.log(error);
+						});
+				}
+			} catch (error) {
+				console.log(error);
+			}
+		}
+
+		getChatMessages();
+		getChatMedia();
+	}, [chat_id]);
+
+	return { conversations, conversationMedia };
 }
