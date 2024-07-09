b7a11cbe80dffa7830fa809f91b904a15f08c5eb
Hairum Qureshi <hqureshi@udel.edu>
2024-07-09 13:03:28 -0400

    updated GET conversation media route so the route contains the conversation ID too

--- backend/routes/conversation.ts
+++ backend/routes/conversation.ts
@@ -1,9 +1,17 @@
 import express from "express";
-import { getAllConversations } from "../controllers/conversation";
+import {
+	getAllConversations,
+	getConversation,
+	getMedia
+} from "../controllers/conversation";
 import { authenticated } from "../middleware/authentication";
 
 const router = express.Router();
 
 router.get("/all", authenticated, getAllConversations);
+
+router.get("/:conversation_id", authenticated, getConversation);
+
+router.get("/:conversation_id/get-media", authenticated, getMedia);
 
 export default router;
