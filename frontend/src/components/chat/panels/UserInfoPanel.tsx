5951ac43fc8e76ce0e12c62fbb60172e72112902
Hairum Qureshi <hqureshi@udel.edu>
2024-07-09 11:41:28 -0400

    resolved uploaded media gallery CSS issue

--- frontend/src/components/chat/panels/UserInfoPanel.tsx
+++ frontend/src/components/chat/panels/UserInfoPanel.tsx
@@ -5,61 +5,87 @@ interface Props {
 }
 
 export default function UserInfoPanel({ otherUser }: Props) {
-	console.log(otherUser);
 	return (
-		<div className="w-1/2 border border-white p-5">
-			<div className="flex items-center justify-center">
-				<img
-					src={otherUser[0].profile_picture}
-					className="w-32 h-32 object-cover border border-blue-400 rounded-lg"
-					alt="User Pfp"
-				/>
+		<div className="border border-blue-500 h-screen w-1/2 bg-slate-800 flex flex-col">
+			<div className="w-full">
+				<div className="flex items-center justify-center m-5 hover:cursor-pointer">
+					<img
+						src={otherUser[0].profile_picture}
+						alt="User pfp"
+						className="w-32 h-32 rounded-lg border border-blue-500 object-cover"
+					/>
+				</div>
 			</div>
-			<h1 className="text-2xl text-center mt-5">{otherUser[0].full_name}</h1>
-			<h2 className="font-semibold text-blue-500 text-center m-2">
+			<div className="text-2xl font-semibold text-center -mt-2">
+				{otherUser[0].full_name}
+			</div>
+			<div className="text-center font-semibold text-purple-400 p-3">
 				{otherUser[0].status_update ||
 					`${
 						otherUser[0].full_name.split(" ")[0]
 					} currently has no status update`}
-			</h2>
-			<div className="text-center my-3">
-				<h1 className="text-xl font-semibold">ABOUT</h1>
-				<div className="border border-white w-full"></div>
-				<p className="text-yellow-400 text-base mt-3">
-					{otherUser[0].biography}
-				</p>
 			</div>
-			<div className="text-center my-2 flex flex-col">
-				<h1 className="text-xl font-semibold">Shared Media</h1>
-				<div className="border border-white w-full flex-grow overflow-y-auto">
-					<div className="h-72 bg-slate-800">
-						<h1 className="p-5 text-lg font-semibold">
-							When you or your friend shares an image, it'll be added here!
-						</h1>
+			<div className="text-center -mt-2 text-yellow-400 p-3">
+				{otherUser[0].biography}
+			</div>
+			<h1 className="text-xl font-semibold text-center mb-2">Uploaded Media</h1>
+			<div className="m-2 flex flex-col flex-grow overflow-auto">
+				<div className="grid grid-cols-2 md:grid-cols-2 gap-4">
+					<div>
+						<img
+							className="h-auto max-w-full rounded-lg"
+							src="https://flowbite.s3.amazonaws.com/docs/gallery/square/image-7.jpg"
+							alt=""
+						/>
 					</div>
-					{/* <div className="h-64 overflow-auto m-4 grid grid-cols-2 gap-2">
+					<div>
 						<img
-							src="https://picsum.photos/200"
-							alt="Uploaded Media"
-							className="w-full h-full object-cover"
+							className="h-auto max-w-full rounded-lg"
+							src="https://flowbite.s3.amazonaws.com/docs/gallery/square/image-8.jpg"
+							alt=""
 						/>
+					</div>
+					<div>
 						<img
-							src="https://picsum.photos/200"
-							alt="Uploaded Media"
-							className="w-full h-full object-cover"
+							className="h-auto max-w-full rounded-lg"
+							src="https://flowbite.s3.amazonaws.com/docs/gallery/square/image-9.jpg"
+							alt=""
 						/>
+					</div>
+					<div>
 						<img
-							src="https://picsum.photos/200"
-							alt="Uploaded Media"
-							className="w-full h-full object-cover"
+							className="h-auto max-w-full rounded-lg"
+							src="https://flowbite.s3.amazonaws.com/docs/gallery/square/image-10.jpg"
+							alt=""
 						/>
+					</div>
+					<div>
 						<img
-							src="https://picsum.photos/200"
-							alt="Uploaded Media"
-							className="w-full h-full object-cover"
+							className="h-auto max-w-full rounded-lg"
+							src="https://flowbite.s3.amazonaws.com/docs/gallery/square/image-11.jpg"
+							alt=""
 						/>
-					</div> */}
+					</div>
+					<div>
+						<img
+							className="h-auto max-w-full rounded-lg"
+							src="https://flowbite.s3.amazonaws.com/docs/gallery/square/image-11.jpg"
+							alt=""
+						/>
+					</div>
+					<div>
+						<img
+							className="h-auto max-w-full rounded-lg"
+							src="https://flowbite.s3.amazonaws.com/docs/gallery/square/image-11.jpg"
+							alt=""
+						/>
+					</div>
 				</div>
+			</div>
+			<div className="m-2">
+				<button className="p-2 border border-green-400 bg-green-700 w-full rounded-md">
+					Save Changes
+				</button>
 			</div>
 		</div>
 	);
