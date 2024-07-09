1d1f3aa00f101bc977650d0921ec6a491a2f648f
Hairum Qureshi <hqureshi@udel.edu>
2024-07-09 15:37:20 -0400

    added logic for updating user data depending on the fields the user had changed from the frontend settings page

--- backend/controllers/user.ts
+++ backend/controllers/user.ts
@@ -17,4 +17,26 @@ const getCurrentUser = async (req: Request, res: Response) => {
 	}
 };
 
-export { getCurrentUser };
+const updateUserSettings = async (req: Request, res: Response) => {
+	const { newFirstName, newLastName, statusUpdate, userBio } = req.body;
+	try {
+		const curr_uid: string = req.cookies.decoded_uid;
+		const updatedData = await User.findByIdAndUpdate(
+			{ _id: curr_uid },
+			{
+				full_name: `${newFirstName} ${newLastName}`,
+				status_update: statusUpdate,
+				biography: userBio || "This user currently does not have a bio written"
+			},
+			{
+				new: true
+			}
+		);
+		res.status(200).json(updatedData);
+	} catch (error) {
+		console.log("<user.ts>".yellow.bold, (error as Error).toString().red.bold);
+		res.status(500).json({ error });
+	}
+};
+
+export { getCurrentUser, updateUserSettings };
