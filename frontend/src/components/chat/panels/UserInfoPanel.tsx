f4f7953e91b9f97a331f74e726b033f7c31418ec
Hairum Qureshi <hqureshi@udel.edu>
2024-07-09 13:13:38 -0400

    implemented logic to map over the selected conversation's uploaded image data in the UserInfoPanel component and display a message to the user if no images have been uploaded

--- frontend/src/components/chat/panels/UserInfoPanel.tsx
+++ frontend/src/components/chat/panels/UserInfoPanel.tsx
@@ -1,10 +1,13 @@
-import { Conversation, ShortUser } from "../../../interfaces";
+import useConversation from "../../../hooks/useConversation";
+import { Conversation, Media, ShortUser } from "../../../interfaces";
 
 interface Props {
 	otherUser: ShortUser[];
 }
 
 export default function UserInfoPanel({ otherUser }: Props) {
+	const { conversationMedia } = useConversation();
+
 	return (
 		<div className="border border-blue-500 h-screen w-1/2 bg-slate-800 flex flex-col">
 			<div className="w-full">
@@ -25,56 +28,31 @@ export default function UserInfoPanel({ otherUser }: Props) {
 						otherUser[0].full_name.split(" ")[0]
 					} currently has no status update`}
 			</div>
-			<div className="text-center -mt-2 text-yellow-400 p-3">
+			<div className="text-center -mt-2 text-gray-400 p-3">
 				{otherUser[0].biography}
 			</div>
 			<h1 className="text-xl font-semibold text-center mb-2">Uploaded Media</h1>
-			<div className="m-2 flex flex-col flex-grow overflow-auto">
-				<div className="grid grid-cols-2 md:grid-cols-2 gap-4">
-					<div>
-						<img
-							className="h-auto max-w-full rounded-lg"
-							src="https://flowbite.s3.amazonaws.com/docs/gallery/square/image-7.jpg"
-							alt=""
-						/>
-					</div>
-					<div>
-						<img
-							className="h-auto max-w-full rounded-lg"
-							src="https://flowbite.s3.amazonaws.com/docs/gallery/square/image-8.jpg"
-							alt=""
-						/>
-					</div>
-					<div>
-						<img
-							className="h-auto max-w-full rounded-lg"
-							src="https://flowbite.s3.amazonaws.com/docs/gallery/square/image-9.jpg"
-							alt=""
-						/>
-					</div>
-					<div>
-						<img
-							className="h-auto max-w-full rounded-lg"
-							src="https://flowbite.s3.amazonaws.com/docs/gallery/square/image-10.jpg"
-							alt=""
-						/>
-					</div>
-					<div>
-						<img
-							className="h-auto max-w-full rounded-lg"
-							src="https://flowbite.s3.amazonaws.com/docs/gallery/square/image-11.jpg"
-							alt=""
-						/>
-					</div>
-					<div>
-						<img
-							className="h-auto max-w-full rounded-lg"
-							src="https://flowbite.s3.amazonaws.com/docs/gallery/square/image-11.jpg"
-							alt=""
-						/>
+			{conversationMedia.length === 0 ? (
+				<p className="p-2 text-slate-500 text-center">
+					When you or your friend share any images, they'll be added right here!
+				</p>
+			) : (
+				<div className="m-2 flex flex-col flex-grow overflow-auto">
+					<div className="grid grid-cols-2 md:grid-cols-2 gap-4">
+						{conversationMedia.map((imgData: Media) =>
+							imgData.images_data.map((imgURL: string) => (
+								<div key={imgURL}>
+									<img
+										className="h-auto max-w-full rounded-lg"
+										src={imgURL}
+										alt="Uploaded Image"
+									/>
+								</div>
+							))
+						)}
 					</div>
 				</div>
-			</div>
+			)}
 		</div>
 	);
 }
