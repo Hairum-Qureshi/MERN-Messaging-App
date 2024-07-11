caca6c3ea042728c1c37d7ffd91db539bf99c810
Hairum Qureshi <hqureshi@udel.edu>
2024-07-11 18:13:24 -0400

    masde Message component take in props from parent Conversation component; replaced placeholder data with data that's being passed in as props, and implemented logic for rendering messages to appear on the right or left depending on if if the current user sent the message or not

--- frontend/src/components/chat/Message.tsx
+++ frontend/src/components/chat/Message.tsx
@@ -1,130 +1,173 @@
 // !NOTE:
 // The last message image does not have a 'mr-2' class
 
-export default function Message() {
+import { Message } from "../../interfaces";
+
+interface Props {
+	message: Message;
+	you: boolean;
+}
+
+export default function Message({ message, you }: Props) {
 	return (
-		<div className="w-full h-5/6 overflow-auto">
-			<div className="m-2 p-2">
-				<div className="flex items-start">
-					<img
-						src="https://i.pinimg.com/736x/e5/9e/51/e59e51dcbba47985a013544769015f25.jpg"
-						className="w-10 h-10 object-cover rounded-md border border-blue-500"
-						alt=""
-					/>
-					<div className="ml-2 border border-blue-500 bg-blue-950 rounded-md p-2 text-sm flex flex-col">
-						<p>
-							Lorem ipsum dolor sit amet consectetur adipisicing elit.
-							Similique, iure natus dolorum placeat cumque, alias quae est
-							possimus ad eveniet tempore blanditiis autem, odio harum deleniti
-							sequi maxime unde quo! Lorem ipsum dolor sit amet consectetur
-							adipisicing elit. Similique, iure natus dolorum placeat cumque,
-							alias quae est possimus ad eveniet tempore blanditiis autem, odio
-							harum deleniti sequi maxime unde quo!
-						</p>
-						<p className="text-xs text-right mt-1">Thu Jul 11, 5:10 PM</p>
-					</div>
-				</div>
-			</div>
-			<div className="m-2 p-2">
-				<div className="flex items-start justify-end">
-					<div className="mr-2 border border-purple-500 bg-purple-950 rounded-md p-2 text-sm flex flex-col">
-						<p>Hi</p>
-						<p className="text-xs text-right mt-1">Thu Jul 11, 5:11 PM</p>
-					</div>
-					<img
-						src="https://i.pinimg.com/736x/e5/9e/51/e59e51dcbba47985a013544769015f25.jpg"
-						className="w-10 h-10 object-cover rounded-md border border-purple-500"
-						alt=""
-					/>
-				</div>
-			</div>
-			<div className="m-2 p-2">
-				<div className="flex items-start justify-end">
-					<div className="mr-2 border border-purple-500 bg-purple-950 rounded-md p-2 text-sm flex flex-col">
-						<p>Here are some uploaded images:</p>
-						<div className="flex items-center mt-1">
-							<img
-								src="https://i.pinimg.com/736x/e5/9e/51/e59e51dcbba47985a013544769015f25.jpg"
-								className="mr-2 w-32 h-32 object-cover rounded-md border border-purple-500"
-								alt=""
-							/>
-							<img
-								src="https://i.pinimg.com/736x/e5/9e/51/e59e51dcbba47985a013544769015f25.jpg"
-								className="mr-2 w-32 h-32 object-cover rounded-md border border-purple-500"
-								alt=""
-							/>
-							<img
-								src="https://i.pinimg.com/736x/e5/9e/51/e59e51dcbba47985a013544769015f25.jpg"
-								className="mr-2 w-32 h-32 object-cover rounded-md border border-purple-500"
-								alt=""
-							/>
-							<img
-								src="https://i.pinimg.com/736x/e5/9e/51/e59e51dcbba47985a013544769015f25.jpg"
-								className="w-32 h-32 object-cover rounded-md border border-purple-500"
-								alt=""
-							/>
+		<div className="w-full">
+			{!you ? (
+				<div className="m-2 p-2">
+					<div className="flex items-start">
+						<img
+							src={message.sender.profile_picture}
+							className="w-10 h-10 object-cover rounded-md border border-blue-500"
+							alt=""
+						/>
+						<div className="ml-2 border border-blue-500 bg-blue-950 rounded-md p-2 text-sm flex flex-col">
+							<p>{message.content}</p>
+							<p className="text-xs text-right mt-1">{message.createdAt}</p>
 						</div>
-						<p className="text-xs text-right mt-1">Thu Jul 11, 5:11 PM</p>
 					</div>
-					<img
-						src="https://i.pinimg.com/736x/e5/9e/51/e59e51dcbba47985a013544769015f25.jpg"
-						className="w-10 h-10 object-cover rounded-md border border-purple-500"
-						alt=""
-					/>
 				</div>
-			</div>
-			<div className="m-2 p-2">
-				<div className="flex items-start">
-					<img
-						src="https://i.pinimg.com/736x/e5/9e/51/e59e51dcbba47985a013544769015f25.jpg"
-						className="w-10 h-10 object-cover rounded-md border border-blue-500"
-						alt=""
-					/>
-					<div className="ml-2 border border-blue-500 bg-blue-950 rounded-md p-2 text-sm flex flex-col">
-						<div className="flex items-center mt-1">
-							<img
-								src="https://i.pinimg.com/736x/e5/9e/51/e59e51dcbba47985a013544769015f25.jpg"
-								className="mr-2 w-32 h-32 object-cover rounded-md border border-blue-500"
-								alt=""
-							/>
-							<img
-								src="https://i.pinimg.com/736x/e5/9e/51/e59e51dcbba47985a013544769015f25.jpg"
-								className="mr-2 w-32 h-32 object-cover rounded-md border border-blue-500"
-								alt=""
-							/>
-							<img
-								src="https://i.pinimg.com/736x/e5/9e/51/e59e51dcbba47985a013544769015f25.jpg"
-								className="mr-2 w-32 h-32 object-cover rounded-md border border-blue-500"
-								alt=""
-							/>
-							<img
-								src="https://i.pinimg.com/736x/e5/9e/51/e59e51dcbba47985a013544769015f25.jpg"
-								className="w-32 h-32 object-cover rounded-md border border-blue-500"
-								alt=""
-							/>
+			) : (
+				<div className="m-2 p-2">
+					<div className="flex items-start justify-end">
+						<div className="mr-2 border border-purple-500 bg-purple-950 rounded-md p-2 text-sm flex flex-col">
+							<p>{message.content}</p>
+							<p className="text-xs text-right mt-1">{message.createdAt}</p>
 						</div>
-						<p className="text-xs text-right mt-1">Thu Jul 11, 5:10 PM</p>
+						<img
+							src={message.sender.profile_picture}
+							className="w-10 h-10 object-cover rounded-md border border-purple-500"
+							alt=""
+						/>
 					</div>
 				</div>
-			</div>
-			<div className="m-2 p-2">
-				<div className="flex items-start justify-end">
-					<div className="mr-2 border border-purple-500 bg-purple-950 rounded-md p-2 text-sm flex flex-col">
-						<p>
-							Lorem ipsum dolor sit amet consectetur adipisicing elit. Quam
-							dolore eius vitae quibusdam impedit vero veniam fugiat sint, illum
-							ipsum dolorem a asperiores exercitationem eaque error repudiandae
-							aliquam, in necessitatibus?
-						</p>
-						<p className="text-xs text-right mt-1">Thu Jul 11, 5:11 PM</p>
-					</div>
-					<img
-						src="https://i.pinimg.com/736x/e5/9e/51/e59e51dcbba47985a013544769015f25.jpg"
-						className="w-10 h-10 object-cover rounded-md border border-purple-500"
-						alt=""
-					/>
-				</div>
-			</div>
+			)}
 		</div>
 	);
 }
+
+// export default function Message({message, you}:Props) {
+// 	return (
+// <div className="w-full">
+// 	<div className="m-2 p-2">
+// 		<div className="flex items-start">
+// 			<img
+// 				src="https://i.pinimg.com/736x/e5/9e/51/e59e51dcbba47985a013544769015f25.jpg"
+// 				className="w-10 h-10 object-cover rounded-md border border-blue-500"
+// 				alt=""
+// 			/>
+// 			<div className="ml-2 border border-blue-500 bg-blue-950 rounded-md p-2 text-sm flex flex-col">
+// 				<p>
+// 					Lorem ipsum dolor sit amet consectetur adipisicing elit.
+// 					Similique, iure natus dolorum placeat cumque, alias quae est
+// 					possimus ad eveniet tempore blanditiis autem, odio harum deleniti
+// 					sequi maxime unde quo! Lorem ipsum dolor sit amet consectetur
+// 					adipisicing elit. Similique, iure natus dolorum placeat cumque,
+// 					alias quae est possimus ad eveniet tempore blanditiis autem, odio
+// 					harum deleniti sequi maxime unde quo!
+// 				</p>
+// 				<p className="text-xs text-right mt-1">Thu Jul 11, 5:10 PM</p>
+// 			</div>
+// 		</div>
+// 	</div>
+// <div className="m-2 p-2">
+// 	<div className="flex items-start justify-end">
+// 		<div className="mr-2 border border-purple-500 bg-purple-950 rounded-md p-2 text-sm flex flex-col">
+// 			<p>Hi</p>
+// 			<p className="text-xs text-right mt-1">Thu Jul 11, 5:11 PM</p>
+// 		</div>
+// 		<img
+// 			src="https://i.pinimg.com/736x/e5/9e/51/e59e51dcbba47985a013544769015f25.jpg"
+// 			className="w-10 h-10 object-cover rounded-md border border-purple-500"
+// 			alt=""
+// 		/>
+// 	</div>
+// </div>
+// 			<div className="m-2 p-2">
+// 				<div className="flex items-start justify-end">
+// 					<div className="mr-2 border border-purple-500 bg-purple-950 rounded-md p-2 text-sm flex flex-col">
+// 						<p>Here are some uploaded images:</p>
+// 						<div className="flex items-center mt-1">
+// 							<img
+// 								src="https://i.pinimg.com/736x/e5/9e/51/e59e51dcbba47985a013544769015f25.jpg"
+// 								className="mr-2 w-32 h-32 object-cover rounded-md border border-purple-500"
+// 								alt=""
+// 							/>
+// 							<img
+// 								src="https://i.pinimg.com/736x/e5/9e/51/e59e51dcbba47985a013544769015f25.jpg"
+// 								className="mr-2 w-32 h-32 object-cover rounded-md border border-purple-500"
+// 								alt=""
+// 							/>
+// 							<img
+// 								src="https://i.pinimg.com/736x/e5/9e/51/e59e51dcbba47985a013544769015f25.jpg"
+// 								className="mr-2 w-32 h-32 object-cover rounded-md border border-purple-500"
+// 								alt=""
+// 							/>
+// 							<img
+// 								src="https://i.pinimg.com/736x/e5/9e/51/e59e51dcbba47985a013544769015f25.jpg"
+// 								className="w-32 h-32 object-cover rounded-md border border-purple-500"
+// 								alt=""
+// 							/>
+// 						</div>
+// 						<p className="text-xs text-right mt-1">Thu Jul 11, 5:11 PM</p>
+// 					</div>
+// 					<img
+// 						src="https://i.pinimg.com/736x/e5/9e/51/e59e51dcbba47985a013544769015f25.jpg"
+// 						className="w-10 h-10 object-cover rounded-md border border-purple-500"
+// 						alt=""
+// 					/>
+// 				</div>
+// 			</div>
+// 			<div className="m-2 p-2">
+// 				<div className="flex items-start">
+// 					<img
+// 						src="https://i.pinimg.com/736x/e5/9e/51/e59e51dcbba47985a013544769015f25.jpg"
+// 						className="w-10 h-10 object-cover rounded-md border border-blue-500"
+// 						alt=""
+// 					/>
+// 					<div className="ml-2 border border-blue-500 bg-blue-950 rounded-md p-2 text-sm flex flex-col">
+// 						<div className="flex items-center mt-1">
+// 							<img
+// 								src="https://i.pinimg.com/736x/e5/9e/51/e59e51dcbba47985a013544769015f25.jpg"
+// 								className="mr-2 w-32 h-32 object-cover rounded-md border border-blue-500"
+// 								alt=""
+// 							/>
+// 							<img
+// 								src="https://i.pinimg.com/736x/e5/9e/51/e59e51dcbba47985a013544769015f25.jpg"
+// 								className="mr-2 w-32 h-32 object-cover rounded-md border border-blue-500"
+// 								alt=""
+// 							/>
+// 							<img
+// 								src="https://i.pinimg.com/736x/e5/9e/51/e59e51dcbba47985a013544769015f25.jpg"
+// 								className="mr-2 w-32 h-32 object-cover rounded-md border border-blue-500"
+// 								alt=""
+// 							/>
+// 							<img
+// 								src="https://i.pinimg.com/736x/e5/9e/51/e59e51dcbba47985a013544769015f25.jpg"
+// 								className="w-32 h-32 object-cover rounded-md border border-blue-500"
+// 								alt=""
+// 							/>
+// 						</div>
+// 						<p className="text-xs text-right mt-1">Thu Jul 11, 5:10 PM</p>
+// 					</div>
+// 				</div>
+// 			</div>
+// 			<div className="m-2 p-2">
+// 				<div className="flex items-start justify-end">
+// 					<div className="mr-2 border border-purple-500 bg-purple-950 rounded-md p-2 text-sm flex flex-col">
+// 						<p>
+// 							Lorem ipsum dolor sit amet consectetur adipisicing elit. Quam
+// 							dolore eius vitae quibusdam impedit vero veniam fugiat sint, illum
+// 							ipsum dolorem a asperiores exercitationem eaque error repudiandae
+// 							aliquam, in necessitatibus?
+// 						</p>
+// 						<p className="text-xs text-right mt-1">Thu Jul 11, 5:11 PM</p>
+// 					</div>
+// 					<img
+// 						src="https://i.pinimg.com/736x/e5/9e/51/e59e51dcbba47985a013544769015f25.jpg"
+// 						className="w-10 h-10 object-cover rounded-md border border-purple-500"
+// 						alt=""
+// 					/>
+// 				</div>
+// 			</div>
+// 		</div>
+// 	);
+// }
