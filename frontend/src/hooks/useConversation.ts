112a256e626ef4fd93f23e0a8e5ab3242dd058f0
Hairum Qureshi <hqureshi@udel.edu>
2024-07-11 18:10:53 -0400

    implemented logic for sending the user's message to the backend and retrieving a conversation's messages data based on chat ID

--- frontend/src/hooks/useConversation.ts
+++ frontend/src/hooks/useConversation.ts
@@ -1,17 +1,20 @@
 import { useEffect, useState } from "react";
 import axios from "axios";
-import { Conversation, Media } from "../interfaces";
+import { Conversation, Media, Message } from "../interfaces";
 
 interface Tools {
 	conversations: Conversation[];
-	// chatMessages: Message[];
+	chatMessages: Message[];
 	conversationMedia: Media[];
+	conversationData: Conversation | undefined;
+	sendMessage: (message: string) => void;
 }
 
 export default function useConversation(): Tools {
 	const [conversations, setConversations] = useState<Conversation[]>([]);
 	const [conversationMedia, setConversationMedia] = useState<Media[]>([]);
-	// const [chatMessages, setChatMessages] = useState<Message[]>([]);
+	const [conversationData, setConversationData] = useState<Conversation>();
+	const [chatMessages, setChatMessages] = useState<Message[]>([]);
 
 	useEffect(() => {
 		async function getChats() {
@@ -31,45 +34,93 @@ export default function useConversation(): Tools {
 	}, []);
 
 	const chat_id: string | undefined = window.location.href.split("/").pop();
-	useEffect(() => {
-		async function getChatMessages() {
-			try {
-				// if (chat_id) {
-				// 	const response = await axios.get(
-				// 		`http://localhost:4000/api/messages/${chat_id}`
-				// 	);
-				// 	setChatMessages(response.data);
-				// }
-			} catch (error) {
-				console.log(error);
-			}
-		}
 
-		async function getChatMedia() {
+	// useEffect(() => {
+	// 	async function getChatMedia() {
+	// 		try {
+	// 			if (chat_id) {
+	// 				await axios
+	// 					.get(
+	// 						`http://localhost:3000/api/conversations/${chat_id}/get-media`,
+	// 						{
+	// 							withCredentials: true
+	// 						}
+	// 					)
+	// 					.then(response => {
+	// 						setConversationMedia(response.data);
+	// 					})
+	// 					.catch(error => {
+	// 						console.log(error);
+	// 					});
+	// 			}
+	// 		} catch (error) {
+	// 			console.log(error);
+	// 		}
+	// 	}
+
+	// 	async function getConversationData() {
+	// 		try {
+	// 			if (chat_id) {
+	// 				await axios
+	// 					.get(`http://localhost:3000/api/conversations/${chat_id}`, {
+	// 						withCredentials: true
+	// 					})
+	// 					.then(response => {
+	// 						setConversationData(response.data);
+	// 					})
+	// 					.catch(error => {
+	// 						console.log(error);
+	// 					});
+	// 			}
+	// 		} catch (error) {
+	// 			console.log(error);
+	// 		}
+	// 	}
+
+	// 	getChatMedia();
+	// 	getConversationData();
+	// }, [chat_id]);
+
+	useEffect(() => {
+		const getChatMessages = async () => {
 			try {
 				if (chat_id) {
-					await axios
-						.get(
-							`http://localhost:3000/api/conversations/${chat_id}/get-media`,
-							{
-								withCredentials: true
-							}
-						)
-						.then(response => {
-							setConversationMedia(response.data);
-						})
-						.catch(error => {
-							console.log(error);
-						});
+					const response = await axios.get(
+						`http://localhost:3000/api/conversations/${chat_id}/messages`
+					);
+					setChatMessages(response.data);
 				}
 			} catch (error) {
 				console.log(error);
 			}
-		}
+		};
 
 		getChatMessages();
-		getChatMedia();
 	}, [chat_id]);
 
-	return { conversations, conversationMedia };
+	async function sendMessage(message: string) {
+		await axios
+			.post(
+				`http://localhost:3000/api/conversations/${chat_id}/send-message`,
+				{
+					message
+				},
+				{
+					withCredentials: true
+				}
+			)
+			.then(response => {
+				setChatMessages(prev => [...prev, response.data]);
+				console.log(response.data);
+			})
+			.catch(error => console.log(error));
+	}
+
+	return {
+		conversations,
+		chatMessages,
+		conversationMedia,
+		conversationData,
+		sendMessage
+	};
 }
