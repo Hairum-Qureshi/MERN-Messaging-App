126e2f7fa47fa9e897853ab3dffe70c074f0f417
Hairum Qureshi <hqureshi@udel.edu>
2024-07-14 17:00:50 -0400

    created a new POST route endpoint for implementing upload image functionality to allow users to change their profile picture

--- backend/routes/user.ts
+++ backend/routes/user.ts
@@ -1,11 +1,23 @@
 import express from "express";
-import { getCurrentUser, updateUserSettings } from "../controllers/user";
+import {
+	getCurrentUser,
+	updateUserSettings,
+	uploadUserProfilePicture
+} from "../controllers/user";
 import { authenticated } from "../middleware/authentication";
+import upload from "../upload";
 
 const router = express.Router();
 
 router.get("/current", authenticated, getCurrentUser);
 
 router.patch("/settings", authenticated, updateUserSettings);
+
+router.post(
+	"/upload",
+	authenticated,
+	upload.single("profile_picture"),
+	uploadUserProfilePicture
+);
 
 export default router;
