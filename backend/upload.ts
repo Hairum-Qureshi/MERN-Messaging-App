f44538abf500b1ac040c5fe86fcbf8c1cadab2fb
Hairum Qureshi <hqureshi@udel.edu>
2024-07-14 16:59:11 -0400

    created file for handling upload logic

---
+++ backend/upload.ts
@@ -0,0 +1,17 @@
+import multer from "multer";
+import path from "path";
+
+const FOLDER_PATH = path.join(__dirname, ".", "/temp_images");
+
+const storage = multer.diskStorage({
+	destination: (req, file, callback) => {
+		callback(null, FOLDER_PATH);
+	},
+	filename: (req, file, callback) => {
+		callback(null, `${Date.now()}-${file.originalname}`);
+	}
+});
+
+const upload = multer({ storage });
+
+export default upload;
