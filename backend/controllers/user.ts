9b20b26447924480659b368071be2679491568da
Hairum Qureshi <hqureshi@udel.edu>
2024-07-14 20:32:02 -0400

    implemented logic for uploaded images to Cloudinary and making the change profile picture functionality work

--- backend/controllers/user.ts
+++ backend/controllers/user.ts
@@ -1,6 +1,20 @@
 import { Request, Response } from "express";
 import User from "../models/user";
 import colors from "colors";
+import { v2 as cloudinary } from "cloudinary";
+import fs from "fs";
+import path from "path";
+import dotenv from "dotenv";
+
+dotenv.config();
+
+cloudinary.config({
+	cloud_name: process.env.CLOUDINARY_NAME,
+	api_key: process.env.CLOUDINARY_API_KEY,
+	api_secret: process.env.CLOUDINARY_SECRET
+});
+
+const FOLDER_PATH = path.join(__dirname, "..", "./temp_images");
 
 colors.enable();
 
@@ -39,4 +53,45 @@ const updateUserSettings = async (req: Request, res: Response) => {
 	}
 };
 
-export { getCurrentUser, updateUserSettings };
+const uploadUserProfilePicture = async (req: Request, res: Response) => {
+	const curr_uid: string = req.cookies.decoded_uid;
+	fs.readdir(FOLDER_PATH, (err, files) => {
+		files.forEach(async file => {
+			const uploadedImagePath = path.resolve(
+				__dirname,
+				`../temp_images/${file}`
+			);
+
+			const uploadResult = await cloudinary.uploader
+				.upload(uploadedImagePath, {
+					public_id: `${curr_uid}-profile_picture`
+				})
+				.catch(error => {
+					console.log(error);
+				});
+
+			try {
+				if (uploadResult) {
+					await User.findByIdAndUpdate(
+						{ _id: curr_uid },
+						{
+							profile_picture: uploadResult.url
+						}
+					);
+
+					fs.unlink(path.join(FOLDER_PATH, file), err => {
+						if (err) throw err;
+					});
+				}
+			} catch (error) {
+				console.log(
+					"<user.ts>".yellow.bold,
+					(error as Error).toString().red.bold
+				);
+				res.status(500).json({ error });
+			}
+		});
+	});
+};
+
+export { getCurrentUser, updateUserSettings, uploadUserProfilePicture };
