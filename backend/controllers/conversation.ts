babc16ee6b6a801c92c4641c57512113df6d28d5
Hairum Qureshi <hqureshi@udel.edu>
2024-07-17 22:42:39 -0400

    modified sendMessage function so it now handles attached images, uploads them to the backend, uploads them to cloudinary, retrieves the URL, saves them to MongoDB, then deletes the images from the backend

--- backend/controllers/conversation.ts
+++ backend/controllers/conversation.ts
@@ -3,8 +3,21 @@ import colors from "colors";
 import Conversation from "../models/chat-related/conversation";
 import mongoose from "mongoose";
 import Message from "../models/chat-related/message";
+import { v2 as cloudinary } from "cloudinary";
+import fs from "fs";
+import path from "path";
+import dotenv from "dotenv";
 
+dotenv.config();
 colors.enable();
+
+cloudinary.config({
+	cloud_name: process.env.CLOUDINARY_NAME,
+	api_key: process.env.CLOUDINARY_API_KEY,
+	api_secret: process.env.CLOUDINARY_SECRET
+});
+
+const FOLDER_PATH = path.join(__dirname, "..", "./temp_images");
 
 async function getAllConversations(req: Request, res: Response) {
 	try {
@@ -79,32 +92,90 @@ async function getConversationMessages(req: Request, res: Response) {
 }
 
 async function sendMessage(req: Request, res: Response) {
-	const { message } = req.body;
+	const curr_uid = req.cookies.decoded_uid;
 	const { conversation_id } = req.params;
-	try {
-		const curr_uid: string = req.cookies.decoded_uid;
-		const createdMessage = await Message.create({
-			sender: curr_uid,
-			conversation_ID: conversation_id,
-			content: message
-		});
+	const { message } = req.body;
 
-		const getCreatedMessage = await Message.findById({
-			_id: createdMessage._id
-		})
-			.populate({
-				path: "sender",
-				select: "_id full_name profile_picture"
-			})
-			.select("-__v -updatedAt");
+	fs.readdir(FOLDER_PATH, async (err, files) => {
+		if (err) {
+			console.log(err);
+			return res.status(500).send("Error reading files");
+		}
 
-		res.status(201).json(getCreatedMessage);
-	} catch (error) {
-		console.log(
-			"<conversation.ts> controller".yellow.bold,
-			(error as Error).toString().red.bold
-		);
-	}
+		const attachments: string[] = [];
+
+		for (const file of files) {
+			const file_parts: string[] = file.split("-");
+			if (file_parts[0] === conversation_id && file_parts[1] === curr_uid) {
+				// Makes sure that only the current user's uploaded images for the specific conversation (by convo ID) are identified and don't get mixed up with any other uploaded files from other users/from different conversations
+				const uploadedImagePath = path.resolve(
+					__dirname,
+					`../temp_images/${file}`
+				);
+
+				try {
+					const uploadResult = await cloudinary.uploader.upload(
+						uploadedImagePath,
+						{
+							public_id: file
+						}
+					);
+
+					if (uploadResult) {
+						attachments.push(uploadResult.url);
+					}
+
+					// Delete the uploaded images from the 'temp_images' folder
+					fs.unlink(path.join(FOLDER_PATH, file), err => {
+						if (err) {
+							return console.log(
+								"<conversation.ts> controller".yellow.bold,
+								(err as Error).toString().red.bold
+							);
+						}
+					});
+
+					if (err) throw err;
+				} catch (error) {
+					console.log(error);
+				}
+			}
+		}
+
+		if (attachments.length > 0) {
+			try {
+				const createdMessage = await Message.create(
+					{
+						sender: curr_uid,
+						conversation_ID: conversation_id,
+						content: message[0],
+						attachments: attachments
+					},
+					{
+						new: true
+					}
+				);
+
+				const getCreatedMessage = await Message.findById({
+					_id: createdMessage[0]._id
+				})
+					.populate({
+						path: "sender",
+						select: "_id full_name profile_picture"
+					})
+					.select("-__v -updatedAt");
+				res.status(201).json(getCreatedMessage);
+			} catch (error) {
+				console.log(
+					"<conversation.ts> controller".yellow.bold,
+					(error as Error).toString().red.bold
+				);
+				return res.status(500).send("Error creating message");
+			}
+		} else {
+			return res.status(400).send("No valid attachments found");
+		}
+	});
 }
 
 export {
